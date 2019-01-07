b7a556eae9595566b7f8cee524667b88b941ee9d
cxw <cxwtomcat@163.com>
2019-01-07 17:16:32 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/home')
-@app.route('/index')
-@app.route('/')
+@app.route('/hello')
+@app.route('/hi')
 def hello():
     return "<h1>Hello Totoro!</h1><img src='http://helloflask.com/totoro.gif'>"
 
@@ -24,4 +23,22 @@ def test_url_for():
     return "test page"
 
 
+name = 'cxw'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
